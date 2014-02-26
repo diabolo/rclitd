b2f4cb4be1c07c185069883ad0e0cc4afc3d1980
Andrew Premdas <apremdas@yahoo.co.uk>
2014-02-26 19:31:44 +0000

    tasks can be added and listed, failing feature

--- lib/todo/task.rb
+++ lib/todo/task.rb
@@ -1,5 +1,16 @@
 module Todo
   class Task
+    FILENAME='~/.todo.txt'
+
+    def self.new_tasks(filename, tasks)
+      File.open(filename, 'a+') do |f|
+        tasks.each do |task|
+          f.puts [task, Time.now].join(',')
+        end
+      end
+
+    end
+
     def initialize(task_name)
     end
   end
